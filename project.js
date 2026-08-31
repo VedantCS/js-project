b98a48d153ff4294a7b1304b6da7b37aa88dfd7d
VedantCS <vedantwate16@gmail.com>
2026-08-31 11:53:34 +0530

    added funtion to decide how many lines to bet on

--- project.js
+++ project.js
@@ -16,6 +16,28 @@ const deposit = () => {
     }
 
 }};
-const depositAmount = deposit();
+
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseInt(lines);
+        if (isNaN(numberOfLines) || numberOfLines < 1 || numberOfLines > 3) {
+            console.log("Invalid number of lines. Please enter a number between 1 and 3.");
+        }
+        else {
+            return numberOfLines; 
+        }
+    }
+
+};
+/*necessary to define functions above before calling them, otherwise it will throw an error
+do not use const depositAmount = deposit(); before defining the function deposit() otherwise it will throw an error
+functions are hoisted but not the variables, so if you define the variable before the function, it will throw an error
+hoisted means that the function is moved to the top of the code, so it can be called before it is defined,*/
+
+const depositAmount = deposit(); 
 console.log(`You have deposited: $${depositAmount}`);
 
+const numberOfLines = getNumberOfLines();
+console.log(`You have chosen to bet on ${numberOfLines} lines.`);
