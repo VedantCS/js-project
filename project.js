6174f60bdd10f40f1d159dbf99da6565c6f35791
VedantCS <vedantwate16@gmail.com>
2026-08-31 12:22:11 +0530

    added bet funtion and changed const to let data type

--- project.js
+++ project.js
@@ -36,8 +36,31 @@ do not use const depositAmount = deposit(); before defining the function deposit
 functions are hoisted but not the variables, so if you define the variable before the function, it will throw an error
 hoisted means that the function is moved to the top of the code, so it can be called before it is defined,*/
 
-const depositAmount = deposit(); 
-console.log(`You have deposited: $${depositAmount}`);
+const getBet = (balance) => {
+while (true) {
+    const bet = prompt("Enter the amount to bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance/numberOfLines) {
+        console.log("Invalid bet amount. Please enter a valid amount.");
+    }
+    else {
+        return numberBet;
+    }
+
+}
+};
+let balance = deposit(); 
+//let data type is used because the balance will change as the user plays the game,
+ //so it needs to be mutable
+ //let keyword is used to declare block-scoped variables that can be changed later
+ //let only exist inside the immediate block {} where they are defined (e.g., inside an if statement, a loop, or a function)
+
+console.log(`You have deposited: $${balance}`);
 
 const numberOfLines = getNumberOfLines();
 console.log(`You have chosen to bet on ${numberOfLines} lines.`);
+
+const bet = getBet(balance, numberOfLines);
+console.log(`You have placed a bet of $${bet}.`);
+
